cfc39bd1095b3f8ddf3ed5e06ec7d6992c9486f0
facu <facundogregori@hotmail.com>
2023-01-23 18:05:39 -0300

    refact canis nft to allow signature mint

--- contracts/CanisNFT.sol
+++ contracts/CanisNFT.sol
@@ -4,12 +4,14 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/common/ERC2981.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+import "./interfaces/ISignatureMintERC721.sol";
 
 /// @title Canis NFT contract
 /// @author Think and Dev
-contract CanisNFT is ERC721URIStorage, ERC2981, Ownable {
+contract CanisNFT is ERC721URIStorage, ERC2981, EIP712, AccessControl {
     /// @dev Max amount of NFTs to be minted
     uint256 public immutable CAP;
     /// @dev Start index of nfts which will be gifted
@@ -25,6 +27,18 @@ contract CanisNFT is ERC721URIStorage, ERC2981, Ownable {
     Counters.Counter private _tokenIdCounter;
     /// @dev Private counter to make internal security checks
     uint256 private tokenIdGiftedIndex;
+
+    /**
+     * @dev Minter rol
+     */
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    using ECDSA for bytes32;
+
+    bytes32 private constant TYPEHASH = keccak256("MintRequest(address to,string uri,uint256 tokenId)");
+
+    /// @dev Mapping from mint request tokenId => whether the mint request is processed.
+    mapping(uint256 => bool) private minted;
 
     event Initialized(
         uint256 cap,
@@ -63,7 +77,7 @@ contract CanisNFT is ERC721URIStorage, ERC2981, Ownable {
         uint256 _startGiftingIndex,
         uint256 _endGiftingIndex,
         string memory _contractUri
-    ) ERC721(name, symbol) {
+    ) ERC721(name, symbol) EIP712(name, "1") {
         require(cap_ > 0, "NFTCapped: cap is 0");
         CAP = cap_;
         startGiftingIndex = _startGiftingIndex;
@@ -71,6 +85,8 @@ contract CanisNFT is ERC721URIStorage, ERC2981, Ownable {
         tokenIdGiftedIndex = startGiftingIndex;
         contractUri = _contractUri;
         super._setDefaultRoyalty(defaultRoyaltyReceiver, defaultFeeNumerator);
+        super._setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        super._setupRole(MINTER_ROLE, msg.sender);
         emit Initialized(
             CAP,
             name,
@@ -102,7 +118,7 @@ contract CanisNFT is ERC721URIStorage, ERC2981, Ownable {
     /// @dev Set royalty receiver and feenumerator to be charged
     /// @param receiver Royalty beneficiary
     /// @param feeNumerator fees to be charged to users on sales
-    function setDefaultRoyalty(address receiver, uint96 feeNumerator) external onlyOwner {
+    function setDefaultRoyalty(address receiver, uint96 feeNumerator) external onlyRole(DEFAULT_ADMIN_ROLE) {
         super._setDefaultRoyalty(receiver, feeNumerator);
         emit DefaultRoyaltyUpdated(receiver, feeNumerator);
     }
@@ -111,7 +127,7 @@ contract CanisNFT is ERC721URIStorage, ERC2981, Ownable {
     /// @dev Canno set a gift index for already minted NFTs
     /// @param startIndex start index
     /// @param endIndex end index
-    function setGiftingIndexes(uint256 startIndex, uint256 endIndex) external onlyOwner {
+    function setGiftingIndexes(uint256 startIndex, uint256 endIndex) external onlyRole(DEFAULT_ADMIN_ROLE) {
         uint256 tokenId = _tokenIdCounter.current();
         require(
             startIndex >= tokenId && startGiftingIndex > endGiftingIndex && endGiftingIndex <= CAP,
@@ -131,7 +147,7 @@ contract CanisNFT is ERC721URIStorage, ERC2981, Ownable {
         uint256 tokenId,
         address receiver,
         uint96 feeNumerator
-    ) external onlyOwner {
+    ) external onlyRole(DEFAULT_ADMIN_ROLE) {
         require(tokenId <= CAP, "CANISNFT: TOKEN ID DOES NOT EXIST");
         super._setTokenRoyalty(tokenId, receiver, feeNumerator);
         emit TokenRoyaltyUpdated(tokenId, receiver, feeNumerator);
@@ -139,7 +155,7 @@ contract CanisNFT is ERC721URIStorage, ERC2981, Ownable {
 
     /// @notice Reset token royalty to default one
     /// @param tokenId Id of the NFT to be modified
-    function resetTokenRoyalty(uint256 tokenId) external onlyOwner {
+    function resetTokenRoyalty(uint256 tokenId) external onlyRole(DEFAULT_ADMIN_ROLE) {
         require(tokenId <= CAP, "CANISNFT: TOKEN ID DOES NOT EXIST");
         super._resetTokenRoyalty(tokenId);
         emit TokenRoyaltyReseted(tokenId);
@@ -147,7 +163,7 @@ contract CanisNFT is ERC721URIStorage, ERC2981, Ownable {
 
     /// @notice Modify a max claim by address
     /// @param max number of new max claim
-    function setMaxClaim(uint256 max) external onlyOwner {
+    function setMaxClaim(uint256 max) external onlyRole(DEFAULT_ADMIN_ROLE) {
         require(max != 0, "CANISNFT: MAX MUST BE GREATER THAN ZERO");
         uint256 oldMax = maxClaim;
         maxClaim = max;
@@ -156,17 +172,17 @@ contract CanisNFT is ERC721URIStorage, ERC2981, Ownable {
 
     /********** INTERFACE ***********/
 
-    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC2981) returns (bool) {
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC2981, AccessControl) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 
     /// @notice Mint NFT
     /// @return id of the new minted NFT
-    function safeMint() public onlyOwner returns (uint256) {
+    function safeMint() public onlyRole(DEFAULT_ADMIN_ROLE) returns (uint256) {
         _tokenIdCounter.increment();
         uint256 newTokenId = _tokenIdCounter.current();
         require(newTokenId <= CAP, "NFTCAPPED: cap exceeded");
-        _safeMint(this.owner(), newTokenId);
+        _safeMint(_msgSender(), newTokenId);
         return newTokenId;
     }
 
@@ -180,7 +196,7 @@ contract CanisNFT is ERC721URIStorage, ERC2981, Ownable {
             "CANISNFT: CANNOT MINT NON GIFTABLE NFT"
         );
         super._approve(to, tokenIdGiftedIndex);
-        super.safeTransferFrom(this.owner(), to, tokenIdGiftedIndex);
+        super.safeTransferFrom(_msgSender(), to, tokenIdGiftedIndex);
         uint256 tokenId = tokenIdGiftedIndex;
         tokenIdGiftedIndex += 1;
         return tokenId;
@@ -188,7 +204,7 @@ contract CanisNFT is ERC721URIStorage, ERC2981, Ownable {
 
     /// @notice Gift an NFT
     /// @param to address to send gifted NFT
-    function gift(address to) external onlyOwner {
+    function gift(address to) external onlyRole(DEFAULT_ADMIN_ROLE) {
         uint256 tokenId = _gift(to);
         emit Gifted(to, tokenId);
     }
@@ -222,14 +238,14 @@ contract CanisNFT is ERC721URIStorage, ERC2981, Ownable {
     /// @notice Set URI for an NFT
     /// @param tokenId id of the NFT to change URI
     /// @param _tokenURI tokenURI
-    function setTokenURI(uint256 tokenId, string memory _tokenURI) external onlyOwner {
+    function setTokenURI(uint256 tokenId, string memory _tokenURI) external onlyRole(DEFAULT_ADMIN_ROLE) {
         super._setTokenURI(tokenId, _tokenURI);
     }
 
     /// @notice Mint NFTs
     /// @param quantity amount of NFTs to be minted
     /// @return id of the next NFT to be minted
-    function safeMintBatch(uint256 quantity) external onlyOwner returns (uint256) {
+    function safeMintBatch(uint256 quantity) external onlyRole(DEFAULT_ADMIN_ROLE) returns (uint256) {
         for (uint256 i = 0; i < quantity; i++) {
             safeMint();
         }
@@ -240,7 +256,7 @@ contract CanisNFT is ERC721URIStorage, ERC2981, Ownable {
     /// @dev The NFTs to be modified has to be consecutives
     /// @param startTokenId index to start modifying NFTs
     /// @param tokenURIs array of modified uris
-    function setTokenURIBatch(uint256 startTokenId, string[] memory tokenURIs) external onlyOwner {
+    function setTokenURIBatch(uint256 startTokenId, string[] memory tokenURIs) external onlyRole(DEFAULT_ADMIN_ROLE) {
         require(tokenURIs.length < 500, "CANISNFT: BATCH SIZE EXCEEDED");
         for (uint256 i = 0; i < tokenURIs.length; i++) {
             super._setTokenURI(startTokenId + i, tokenURIs[i]);
@@ -249,8 +265,57 @@ contract CanisNFT is ERC721URIStorage, ERC2981, Ownable {
 
     /// @notice Modify contractUri for NFT collection
     /// @param _contractUri contractUri
-    function setContractURI(string memory _contractUri) external onlyOwner {
+    function setContractURI(string memory _contractUri) external onlyRole(DEFAULT_ADMIN_ROLE) {
         contractUri = _contractUri;
         emit ContractURIUpdated(contractUri);
+    }
+
+    /// @dev Verifies that a mint request is signed by an authorized account.
+    function verify(ISignatureMintERC721.MintRequest calldata _req, bytes calldata _signature)
+        public
+        view
+        returns (bool success, address signer)
+    {
+        signer = _recoverAddress(_req, _signature);
+        success = !minted[_req.tokenId] && _canSignMintRequest(signer);
+    }
+
+    /// @dev Returns whether a given address is authorized to sign mint requests.
+    function _canSignMintRequest(address _signer) internal view returns (bool) {
+        require(hasRole(MINTER_ROLE, _signer), "CANISNFT: must have minter role to mint");
+        return true;
+    }
+
+    /// @dev Verifies a mint request and marks the request as minted.
+    function _processRequest(ISignatureMintERC721.MintRequest calldata _req, bytes calldata _signature)
+        internal
+        returns (address signer)
+    {
+        bool success;
+        (success, signer) = verify(_req, _signature);
+
+        if (!success) {
+            revert("CANISNFT: Invalid request");
+        }
+
+        require(_req.to != address(0), "CANISNFT: recipient undefined");
+        require(_req.tokenId <= CAP, "CANISNFT: cap exceeded");
+        require(_req.tokenId <= _tokenIdCounter.current(), "CANISNFT: Invalid token ID");
+
+        minted[_req.tokenId] = true;
+    }
+
+    /// @dev Returns the address of the signer of the mint request.
+    function _recoverAddress(ISignatureMintERC721.MintRequest calldata _req, bytes calldata _signature)
+        internal
+        view
+        returns (address)
+    {
+        return _hashTypedDataV4(keccak256(_encodeRequest(_req))).recover(_signature);
+    }
+
+    /// @dev Resolves 'stack too deep' error in `recoverAddress`.
+    function _encodeRequest(ISignatureMintERC721.MintRequest calldata _req) internal pure returns (bytes memory) {
+        return abi.encode(TYPEHASH, _req.to, keccak256(bytes(_req.uri)), _req.tokenId);
     }
 }
