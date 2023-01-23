96d1dfb9392048d2ad839059e34ee8cd9c3d28b0
facu <facundogregori@hotmail.com>
2023-01-23 15:08:14 -0300

    refact deploy scripts

--- deploy/01_Royalty.js
+++ deploy/01_Royalty.js
@@ -11,13 +11,20 @@ module.exports = async (hardhat) => {
 
   const chainId = parseInt(await getChainId(), 10)
   const {
-    royaltyReceiver
-  } = config[contractName][chainId]
+    royaltyReceiver,
+    percentageReceiver,
+    percentageUBI
+  } = config['Royalty'][chainId]
+
+  const defaultSwapBurner = (await ethers.getContract('SwapBurner', this.deployer)).address
+
+  const constructorArguments = [[royaltyReceiver, defaultSwapBurner], [percentageReceiver, percentageUBI]]
 
   const isTestEnvironment = chainId === 31337 || chainId === 1337
 
   const signer = await ethers.provider.getSigner(deployer)
-
+  const payees = ["0x5Bf2b91D1e4aD605ABf558F58490Bb17fa59397E"]
+  const shares = ["1000000000000000"]
   dim('\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
   dim(`Blockchain Canis Contracts - Deploy ${contractName}`)
   dim('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
@@ -27,7 +34,7 @@ module.exports = async (hardhat) => {
 
   cyan(`\nDeploying ${contractName}...`)
   const result = await deploy(contractName, {
-    args: [royaltyReceiver],
+    args: constructorArguments,
     contract: contractName,
     from: deployer,
     skipIfAlreadyDeployed: false
@@ -39,7 +46,7 @@ module.exports = async (hardhat) => {
   green('Contract Deployments Complete!')
   dim('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
 
-  await verifyContract(network, result, contractName, [royaltyReceiver])
+  await verifyContract(network, result, contractName, constructorArguments)
 
   return true
 }
